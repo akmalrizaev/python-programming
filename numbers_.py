5ab04552fdee18ac0c5bf3566b46170542a4be40
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2024-12-07 15:16:46 +0500

    Numbers

---
+++ numbers_.py
@@ -0,0 +1,38 @@
+import math
+
+x = 10
+x = 0b10
+print(x)
+print(bin(x))
+
+x = 0x12c
+print(x)
+print(hex(x))
+
+# complex numbers
+# a + bi
+
+x = 1 + 2j
+
+# arithmetic operations
+x = 10 + 3
+x = 10 - 3
+x = 10 * 3
+x = 10 / 3
+x = 10 // 3        # 3
+x = 10 % 3
+x = 10 ** 3  # 1000
+
+PI = - 3.14
+print(round(PI))
+print(abs(PI))
+print(math.floor(PI))
+
+# Type Conversion
+
+x = input("x: ")
+y = x + 1
+print(int(x))
+print(float(x))
+print(bool(x))
+print(str(x))
