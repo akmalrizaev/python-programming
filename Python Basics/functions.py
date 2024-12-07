c26f50b7305a81fa211133123aeb8eac8e0917e9
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2024-12-07 18:29:56 +0500

    Functions arguments

--- Python Basics/functions.py
+++ Python Basics/functions.py
@@ -10,3 +10,24 @@ def decrement(number: int, by: int = 1) -> tuple:
 
 
 print(decrement(5))
+
+# Arguments - *args
+
+
+def multiply(*list):
+    total = 1
+    for number in list:
+        total *= number
+    return total
+
+
+print(multiply(2, 3, 4, 5))
+
+# Arguments- **args
+
+
+def save_user(**user):
+    print(user["name"])
+
+
+save_user(id=1, name="admin")
