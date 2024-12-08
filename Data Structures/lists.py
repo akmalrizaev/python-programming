947b83a9945f5d0baf862ecc44bed8bebfb2afb1
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2024-12-08 16:27:02 +0500

    Data Structure - Lists

---
+++ Data Structures/lists.py
@@ -0,0 +1,14 @@
+letters = ["a", "b", "c"]
+
+matrix = [[0, 1], [2, 3]]
+
+zeros = [0] * 5
+
+combined = zeros + letters
+
+numbers = list(range(20))
+
+chars = list("Hello Wrld")
+
+print(chars)
+print(len(chars))
