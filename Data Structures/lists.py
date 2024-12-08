00a60f123a0919ede384e41771c903bc3f686b8b
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2024-12-08 18:50:35 +0500

    Finding Items and Sorting Lists

--- Data Structures/lists.py
+++ Data Structures/lists.py
@@ -52,3 +52,30 @@ letters.remove("b")
 del letters[0:3]
 letters.clear()
 print(letters)
+
+# Finding Items
+letters = ["a", "b", "c"]
+print(letters.count("d"))
+if "d" in letters:
+    print(letters.index("d"))
+
+# Sorting Lists
+numbers = [3, 51, 2, 8, 6]
+numbers.sort()
+numbers.sort(reverse=True)
+sorted(numbers)
+sorted(numbers, reverse=True)
+
+items = [
+    ("Product1", 10),
+    ("Product2", 9),
+    ("Product1", 12),
+]
+
+
+def sort_item(item):
+    return item[1]
+
+
+items.sort(key=sort_item)
+print(items)
