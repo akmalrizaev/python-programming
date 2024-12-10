d259bf8d3613dafa3ff4d54c54b645c8107af1a7
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2024-12-10 18:42:42 +0500

    Raising Exeptions

--- Exceptions/exceptions.py
+++ Exceptions/exceptions.py
@@ -49,3 +49,15 @@ except (ValueError, ZeroDivisionError):
     print("You didn't enter a valid age.")
 else:
     print("No exceptions were thrown.")
+
+
+def calculate_xfactor(age):
+    if age <= 0:
+        raise ValueError("Age cannot be 0 or less.")
+    return 10 / age
+
+
+try:
+    calculate_xfactor(-1)
+except ValueError as error:
+    print(error)
