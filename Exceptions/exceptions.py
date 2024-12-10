7135c10e5768a1368cda1db8e99cf82a2f4aa95e
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2024-12-10 18:26:57 +0500

    adding finally block

--- Exceptions/exceptions.py
+++ Exceptions/exceptions.py
@@ -25,3 +25,15 @@ except (ValueError, ZeroDivisionError):
     print("You didn't enter a valid age.")
 else:
     print("No exceptions were thrown.")
+
+
+try:
+    file = open("app.py")
+    age = int(input("Age: "))
+    xfactor = 10 / age
+except (ValueError, ZeroDivisionError):
+    print("You didn't enter a valid age.")
+else:
+    print("No exceptions were thrown.")
+finally:
+    file.close()
