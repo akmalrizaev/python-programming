2fc8d6b6eb0efe36f68ae3dd95384d7c509d48c2
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2024-12-10 18:33:29 +0500

    The With Statement

--- Exceptions/exceptions.py
+++ Exceptions/exceptions.py
@@ -37,3 +37,15 @@ else:
     print("No exceptions were thrown.")
 finally:
     file.close()
+
+
+try:
+    with open("app.py") as file:
+        print("File opened.")
+
+    age = int(input("Age: "))
+    xfactor = 10 / age
+except (ValueError, ZeroDivisionError):
+    print("You didn't enter a valid age.")
+else:
+    print("No exceptions were thrown.")
