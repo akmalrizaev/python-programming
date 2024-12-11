06a0acc8705b610f0d7461f888e380a8b2dedc73
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2024-12-11 15:45:30 +0500

    Comparing Objects

--- Classes/magic-methods.py
+++ Classes/magic-methods.py
@@ -8,9 +8,17 @@ class Point:
     def __str__(self):
         return f"({self.x}, {self.y})"
 
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __gt__(self, other):
+        return self.x > other.x and self.y > other.y
+
     def draw(self):
         print(f"Point ({self.x}, {self.y})")
 
 
 point = Point(1, 2)
+other = Point(1, 2)
 print(point)
+print(point == other)
