d293de5557aa2c72b39a7acc941ff0f2f88d70c0
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2024-12-11 09:17:07 +0500

    Class vs Instance Attributes

--- Classes/classes.py
+++ Classes/classes.py
@@ -1,4 +1,6 @@
 class Point:
+    default_color = "red"
+
     def __init__(self, x, y):
         self.x = x
         self.y = y
@@ -7,9 +9,16 @@ class Point:
         print(f"Point ({self.x}, {self.y})")
 
 
+Point.default_color = "yellow"
 point = Point(1, 2)
 print(type(point))
 print(isinstance(point, Point))
+print(point.default_color)
+print(Point.default_color)
 
 print(point.x)
 point.draw()
+
+another = Point(3, 4)
+print(another.default_color)
+another.draw()
